c71ca12e063d8f39b5a771203474de35a456e644
Gustavo Silva <92289607+GugaS1lva@users.noreply.github.com>
2022-03-26 05:44:01 -0300

    JS - [Básico]

---
+++ 07-Modulo-JS-[Básico]/01-primeiros-comandos/index.js
@@ -0,0 +1,9 @@
+console.log('Olá, Mundo!')
+
+var msg = window.document.getElementsByClassName('msgBox')[0]
+
+msg.style.background = 'black'
+msg.style.color = 'white'
+msg.style.padding = '15px'
+
+msg.innerHTML = 'Olá, Mundo!!!'
