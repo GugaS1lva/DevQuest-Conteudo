13751f728ff22594b8bc210ef6cf431ec0dd3e06
Gustavo Silva <92289607+GugaS1lva@users.noreply.github.com>
2022-03-27 06:05:54 -0300

    JS - [Básico]

---
+++ 07-Modulo-JS-[Básico]/03-variaveis/index.js
@@ -0,0 +1,31 @@
+/* 
+    - Deve usar Nomes bem explicativos nas variáveis.
+    - Letras maiúscolas e minúscolas FAZEM DIFERENÇA
+    - Não pode criar variáveis com BackSpace
+    - Pode usar acentos nas variáveis (mas não é aconselhável)
+*/
+
+//variável do tipo <string>
+var nomePessoa = 'Guga' 
+
+
+//VARIÁVEL CONSTANTE: Valor INALTERÁVEL
+const emailPessoa = 'gugasilva017@outlook.com'
+
+
+// Aceita REATRIBUIÇÃO: Valor Alterável
+let telefonePessoa = '8240028922'
+
+
+/* ESCOPO:
+    "Lugar que aquela variável vai existir."
+
+    No CSS tem: variáveis de escopo global e local
+    No JS tem: Global, Função e Bloco.
+
+        ● Escopo Global [] > Variáveis que você pode usar em qualquer lugar do código
+
+        ● Escopo de Função [] > Variáveis que só podem ser usadas dentro de uma função
+
+        ● Escopo de Bloco de código [] > Variáveis que só vão poder ser usadas dentro dos Blocos de Código
+*/ 
