5fc37fedecbb8a1e36c5c7fd2a1832d5e32eb6e8
Gustavo Silva <92289607+GugaS1lva@users.noreply.github.com>
2022-03-28 21:48:21 -0300

    JS - [Básico]

---
+++ 07-Modulo-JS-[Básico]/04-tipos-primitivos/index.js
@@ -0,0 +1,22 @@
+// STRING > Representa um dado do tipo texto e pode ser ecrito tanto com àspas simples quanto àspas duplas.
+let nome = 'Guga'
+let numero = '1'
+let teste = '@35235amsfp6;][[]~251.61651@#$%¨&*()__3-'
+
+// NUMBER > Representa um dado do tipo number.
+let num = 1234
+let num2 = 124.56
+let num3 = 0.31543
+let num4 = -3.14
+
+// BOOLEAN > Representa um dado verdadeiro ou falso.
+let usuario01 = true
+let usuario02 = false
+
+// NULL > Representa um dado inválido ou inexistente.
+let pessoa = null
+
+// UNDEFINED > Representa um impedimento do JavaScript de definir um tipo para a variável.
+    // Geralmente o JS atribui valor indefinido à alguma variável de forma automática.
+let telefone = undefined
+
