d836e69678110f44eba7de355d2acba8beb2dea7
Gustavo Silva <92289607+GugaS1lva@users.noreply.github.com>
2022-03-31 05:44:00 -0300

    JS - [Básico]

---
+++ 07-Modulo-JS-[Básico]/09-lacos-02/index.js
@@ -0,0 +1,17 @@
+/* Anatomia do 'while'
+
+    while (teste-lógico){
+        bloco
+    }
+
+    // while - "Enquanto o teste-lógico for verdadeiro, execute o bloco de código. Quando não, finalize o laço.    
+*/
+
+let a = 6
+let b = 10
+
+console.log('Continue caminhando, não desista!!')
+while(a <= b){
+    console.log(`Dê agora o passo ${a}`)
+    a++
+}
