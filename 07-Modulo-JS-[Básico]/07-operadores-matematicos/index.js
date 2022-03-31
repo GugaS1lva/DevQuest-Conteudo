c5e97be116073b3ab5696083ab18d392bc227e58
Gustavo Silva <92289607+GugaS1lva@users.noreply.github.com>
2022-03-31 05:07:45 -0300

    JS - [Básico]

---
+++ 07-Modulo-JS-[Básico]/07-operadores-matematicos/index.js
@@ -0,0 +1,17 @@
+//function divisao01(){
+    let primeiroValor = 10
+    let segundoValor = 2
+
+    let soma = primeiroValor + segundoValor
+    let subtracao = primeiroValor - segundoValor
+    let divisaoReal = primeiroValor / segundoValor
+    let divisaoInteira = primeiroValor % segundoValor //RESTO DA DIVISÃO
+    let multiplicacao = primeiroValor * segundoValor
+    
+    
+    console.log(`A SOMA entre ${primeiroValor} e ${segundoValor} é igual a ${soma}`)
+    console.log(`A SUBTRAÇÃO entre ${primeiroValor} e ${segundoValor} é igual a ${subtracao}`)
+    console.log(`A DIVISÃO-REAL entre ${primeiroValor} e ${segundoValor} é igual a ${divisaoReal}`)
+    console.log(`A DIVISÃO-INTEIRA entre ${primeiroValor} e ${segundoValor} é igual a ${divisaoInteira}`)
+    console.log(`A MULTIPLICAÇÃO entre ${primeiroValor} e ${segundoValor} é igual a ${multiplicacao}`)
+//}
