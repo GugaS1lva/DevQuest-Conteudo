71f43734a58bab7ca04c818a6e72c3ad5b5ec242
Gustavo Silva <92289607+GugaS1lva@users.noreply.github.com>
2022-03-31 05:29:10 -0300

    JS - [Básico]

---
+++ 07-Modulo-JS-[Básico]/08-lacos-01/index.js
@@ -0,0 +1,17 @@
+/* Anatomia do 'for'
+
+    for (inicializador; teste-lógico; incremento){
+        bloco
+    }
+
+    // EXEMPLO
+        inicializador: uma variável é criada e recebe um valor inicial.
+        teste-lógico: SE verdadero executa o bloco
+        incremento: no fim da execução do bloco, faz um incremento na variável e retorna ao laço, fazendo novamente o teste-lógico. Quando o teste-lógico resultar em falso, pula o bloco de código e finaliza o laço de repetição.
+*/
+
+
+console.log('Vamos caminhar!')
+for (let passo = 1; passo <= 5; passo++){
+    console.log(`Dê agora o passo ${passo}.`)
+}
