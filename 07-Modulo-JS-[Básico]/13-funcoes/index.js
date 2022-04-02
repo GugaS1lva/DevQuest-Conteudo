6bc713ba420e44a84912fc41a3a22773625d0f5a
Gustavo Silva <92289607+GugaS1lva@users.noreply.github.com>
2022-04-02 04:02:25 -0300

    JS - [Básico]

---
+++ 07-Modulo-JS-[Básico]/13-funcoes/index.js
@@ -0,0 +1,43 @@
+/* ANATOMIA DE UMA FUNÇÃO - Sem Parâmetros
+
+    function nomeDaFunção(){
+        bloco
+    } 
+
+    nomeDaFunção()
+*/
+/* ANATOMIA DE UMA FUNÇÃO - Com Parâmetros
+
+    function nomeDaFunção(parâmetro01, parâmetro02){
+        bloco
+    } 
+
+    nomeDaFunção(parâmetro01, parâmetro02)
+*/
+
+/*
+function incentivarQuester(){
+    window.alert('Você chegou ao JS, PARABÉNS!!!')
+}
+incentivarQuester()
+*/ 
+
+
+/* Mostra a soma direto no console:
+
+    function soma(num1, num2){
+        console.log(num1 + num2)
+    }
+
+    soma(5, 7)
+*/
+
+/* Joga o resultado do return direto pra quem tá chamando:
+
+    function soma(num1, num2){
+        return num1 + num2
+    }
+
+    soma(5, 7)
+*/
+
