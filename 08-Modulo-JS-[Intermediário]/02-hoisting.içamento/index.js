c2bd6fb0b3b9fb854d63d73f6dc103bea3775849
Gustavo Silva <92289607+GugaS1lva@users.noreply.github.com>
2022-04-14 07:56:24 -0300

    JS - [Intermediário]

---
+++ 08-Modulo-JS-[Intermediário]/02-hoisting.içamento/index.js
@@ -0,0 +1,12 @@
+let nome
+
+console.log(nome)
+nome = 'Guga'
+
+
+/* PRECISA DE UMA ORDEM:
+    Primeiro a DECLARAÇÃO + ATRIBUIÇÃO para depois usar a variável normalmente.
+        Caso a DECLARAÇÃO seja feita sem a ATRIBUIÇÃO, o JavaScript incrementa um valor indefinido ('UNDEFINED') para a variável e mostra na tela.
+
+        Caso a DECLARAÇÃO não seja feita, o JavaScript retorna um erro sintático.
+*/
