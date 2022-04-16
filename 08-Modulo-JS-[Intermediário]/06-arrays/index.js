ce910dff6f278419fdd347ee927252fb0c86c5bd
Gustavo Silva <92289607+GugaS1lva@users.noreply.github.com>
2022-04-16 05:37:09 -0300

    JS - [Intermediário]

---
+++ 08-Modulo-JS-[Intermediário]/06-arrays/index.js
@@ -0,0 +1,14 @@
+// Array: É uma variável capaz de guardar vários dados de diversos tipos dentro de simesma. Uma variável que guarda outras coisas dentro, inclusive outros Arrays.
+
+
+let notas = [1, 2, 3, 4, 5]
+let se7e = '7'
+let filmes = ['Interestelar', 'Shrek 1', 'Shrek 2', 'Shrek Terceiro', 'Shrek Para Sempre', notas, se7e ]
+
+
+// Acessando todos os elementos do Array:
+    console.log(filmes)
+
+// Acessando 1 Elemento do Array: //OBS: Array sempre começa em 0
+    console.log( filmes[5] )
+
