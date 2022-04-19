000d32e0258010758c9aa7c8cca9fb52cb5ff30a
Gustavo Silva <92289607+GugaS1lva@users.noreply.github.com>
2022-04-19 07:54:28 -0300

    JS [Intermediário]

---
+++ 08-Modulo-JS-[Intermediário]/12-this/thisArrowFunction.js
@@ -0,0 +1,3 @@
+/* Arrow Functions não possuem um 'This' próprio.
+    Caso você use um 'this' dentro de uma Arrow Function, o valor desse 'this' vai ser herdado do 'this' do escopo que envolve a declaração da Arrow Function.
+*/
