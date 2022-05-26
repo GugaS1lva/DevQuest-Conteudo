e83dc120f64dc6e40bb0fc37fd98dea3ba034b85
Gustavo Silva <92289607+GugaS1lva@users.noreply.github.com>
2022-05-26 11:01:18 -0300

    JS - [Avançado]

---
+++ 09-Modulo-JS-[Avançado]/19-modulos-javascript/index.js
@@ -0,0 +1,4 @@
+import {somar, subtrair, multiplicar, dividir, duplicar, triplicar, quadruplicar} from "./operacoes.js"
+
+console.log(somar(2, 5))
+console.log(triplicar(30000))
---
+++ 09-Modulo-JS-[Avançado]/19-modulos-javascript/operacoes.js
@@ -0,0 +1,24 @@
+function somar(n1, n2){
+    return n1 + n2
+}
+function subtrair(n1, n2){
+    return n1 - n2
+}
+function multiplicar(n1, n2){
+    return n1 * n2
+}
+function dividir(n1, n2){
+    return n1 / n2
+}
+
+function duplicar(n1){
+    return n1 * 2
+}
+function triplicar(n1){
+    return n1 * 3
+}
+function quadruplicar(n1){
+    return n1 * 4
+}
+
+export {somar, subtrair, multiplicar, dividir, duplicar, triplicar, quadruplicar}
